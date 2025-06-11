869fcca3f1ea7a3ec53707156ff5744a7e3a14f0
KPLAY <utd888@gmail.com>
2025-06-11 23:42:09 +0700

    Add files via upload

---
+++ register.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAxB3tZKz-WiEI_kqOP5hTA6gVgpznx3ck",
+  authDomain: "registertour-cd779.firebaseapp.com",
+  projectId: "registertour-cd779",
+  storageBucket: "registertour-cd779.firebasestorage.app",
+  messagingSenderId: "986176690425",
+  appId: "1:986176690425:web:2146d25fb85313b41e6a28",
+  measurementId: "G-54KYND25LT"
+};
